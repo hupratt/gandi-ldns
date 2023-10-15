19c13a86982e94fb77d20ae31ed51aca3df4ca65
hupratt <hprattdo@gmail.com>
2023-10-15 16:52:07 +0200

    mistake in bash

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-/home/ubuntu/Dev/gandi-ldns/env/bin/python /home/ubuntu/Dev/gandi-ldns/gandi-ldns
+/home/ubuntu/Dev/gandi-ldns/env/bin/python /home/ubuntu/Dev/gandi-ldns/gandi-ldns.py
